2530d98b85bb0d8c77e26ddc35b0d3af43347ba2
kimsungkwang <skk405@naver.com>
2022-08-30 15:28:45 +0900

    비동기 콜백함수

--- ex05.js
+++ ex05.js
@@ -1,11 +1,22 @@
 function taskA(a, b, callback) {
   setTimeout(() => {
     const res = a + b;
-    callback(res)
+    callback(res);
   }, 5000);
 }
 
+function taskB(a, callback) {
+  setTimeout(() => {
+    const res = a * 2;
+    callback(res);
+  }, 6000);
+}
+
 taskA(1, 5, (res) => {
-  console.log("A Task RESULT : ", res)
+  console.log("A Task RESULT : ", res);
+});
+
+taskB(8, (res) => {
+  console.log("B TASK RESULT : ", res);
 });
 console.log("End");
